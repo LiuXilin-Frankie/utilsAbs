2b258dd8e36db855bad8ad14ccec7147e2719567
AbsoluteX <118020043@link.cuhk.edu.cn>
2023-03-14 14:26:03 +0800

    11

--- PlotRet.py
+++ PlotRet.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # plotting functions
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -19,26 +18,4 @@ def CalSR(series):
     Er = (series.mean()-1)
     Estd = (series.std()) * (252**0.5) 
     SR = (Er/Estd)
-=======
-# plotting functions
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import pandas as pd
-import numpy as np
-
-def plotNetValue(df,Xname,Yname):
-    print('SharpeRatio:',CalSR(df[Yname]))
-    plt.figure(figsize=(16,4))
-    XaxisNum = df.shape[0]//20
-    plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(XaxisNum))
-    plt.xticks(rotation=30)
-    plt.plot(df[Xname],df[Yname])
-    plt.show()
-
-def CalSR(series):
-    Er = (series.mean()-1)
-    Estd = (series.std()) * (252**0.5) 
-    SR = (Er/Estd)
->>>>>>> c07dc95adf606d0241228d00170438bc904fc3c0
     return float(str(SR)[:5])
